adc16550a26b22a0082660e8e9640f02a5c0bb06
Ermal Leka <ermalleka@Ermals-Air.lan>
2021-11-05 17:26:27 -0400

    Class 1 code

--- src/class1/File1.java
+++ src/class1/File1.java
@@ -2,7 +2,11 @@ package class1;
 
 public class File1 {
     public static void main(String[] args) {
-        System.out.println("HELLO MALI" +
-                "");
+        // store number of days in an year
+        short noOfDaysInYear = 365;
+        short currentYear = 2021;
+
+        System.out.println("Number of days in an year ->" +noOfDaysInYear);
+        System.out.println("There are " + noOfDaysInYear + " days in year " + currentYear);
     }
 }
---
+++ src/class1/File2.java
@@ -0,0 +1,11 @@
+package class1;
+
+public class File2 {
+    public static void main(String[] args) {
+
+        // store number of days in a year
+        System.out.println("number of days in a year");
+    }
+
+
+}
